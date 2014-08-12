18f987b013a1b8910496343dd071057c71758dcd
Joshua Kemp <josh.kemp@ziplist.com>
2014-08-11 21:30:00 -0400

    Epicodus bootcamp coursework starting Week 3

--- Cats VS Dogs/js/scripts.js
+++ Cats VS Dogs/js/scripts.js
@@ -1,5 +1,27 @@
-$(document).click(function() {
-  $('#c_img').click(function() {
-    alert('HI!');
+$(document).ready(function() {
+  $('img#c_img').click(function() {
+    $('ul#cat').append("<li>Meow! <span class='clickable'>[ x ]</span></li>");
+    $('ul#dog').append("<li>BARK! BARK! BARK! <span class='clickable'>[ x ]</span></li>");
+
+    $('ul#cat').children("li").last().click(function() {
+      $(this).remove();
+    });
+    $('ul#dog').children('li').last().click(function() {
+      $(this).remove();
+    });
   });
+
+
+  $('#d_img').click(function() {
+    $('ul#dog').append("<li>BARK! BARK! BARK! <span class='clickable'>[ x ] </span></li>");
+    $('ul#cat').append("<li> Meow! <span class='clickable'>[ x ] </span></li>");
+
+    $('ul#cat').children("li").last().click(function() {
+      $(this).remove();
+    });
+    $('ul#dog').children('li').last().click(function() {
+      $(this).remove();
+    });
+  });
+
 });
