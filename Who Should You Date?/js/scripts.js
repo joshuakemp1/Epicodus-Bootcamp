a80be43ca27eb198fe8e386eaf4a84e973340410
Joshua Kemp <josh.kemp@ziplist.com>
2014-08-17 13:40:03 -0400

    Starting Javacript Epicodus bootcamp coursework

---
+++ Who Should You Date?/js/scripts.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {
+  $('#quote').hide();
+});
--- if and else/js/scripts.js
+++ if and else/js/scripts.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-  if (confirm("Do you want to see our programming laguage options? Click OK for yes or Cancel for no.")) {
+
+  var p_lang = (prompt("Do you want to see our programming laguage options? Please type in 'yes' or 'no' ." ));
+
+
+  $('body').addClass('body_black');
+  $('#javascript').hide();
+  $('.container-fluid h1').hide();
+  $('.yell').hide();
+
+
+  if (p_lang === 'yes') {
     $('#javascript').show();
-  } else {
-    $('body').addClass('body_black');
-    $('#no_show').addClass('yellow');
+  } else if (p_lang === 'no') {
+    $('.yell').show();
   }
+   else {
+    alert('YO DUMMY!');
+  }
+
+
 });
