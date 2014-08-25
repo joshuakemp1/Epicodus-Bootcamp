79fa6c22871e4a7fe32a4967af55983e0dff9773
Joshua Kemp <josh.kemp@ziplist.com>
2014-08-25 10:17:29 -0400

    Epicodus coursework

---
+++ Tic Tac Toe Practice/spec/specs.js
@@ -0,0 +1,17 @@
+describe('leapYear', function() {
+  it("is false for a year that is not divisible by 4", function() {
+    leapYear(1999).should.equal(false);
+  });
+
+  it("is true for most years divisible by 4", function() {
+    leapYear(2012).should.equal(true);
+  });
+
+  it("is false for most years divisible by 100", function(){
+    leapYear(1900).should.equal(false);
+  });
+
+  it("is true for a year that is divisible by 400", function() {
+    leapYear(2000).should.equal(true);
+ });
+});
